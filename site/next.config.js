6fb00cd0d7997b79802cf6fc88ce76b905204dda
DjSni <31524614+DjSni@users.noreply.github.com>
2025-02-14 20:02:02 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Snis Kasm Registry',
+    description: 'My Store of Kasm workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://djsni.github.io/kasm_workspaces_registry/',
+    contactUrl: 'https://github.com/DjSni/kasm_workspaces_registry',
   },
   reactStrictMode: true,
   swcMinify: true,
