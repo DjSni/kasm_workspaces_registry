3306b9ef408224b5795206cca45f21c74eec7ebe
DjSni <github-action@actions-template-sync.noreply.github.com>
2025-02-14 19:12:31 +0000

    chore(template): merge template changes :up:
    Signed-off-by: DjSni <github-action@actions-template-sync.noreply.github.com>

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Snis Kasm Registry',
-    description: 'My Store of Kasm workspaces.',
+    name: 'Kasm Technologies',
+    description: 'The official store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://djsni.github.io/kasm_workspaces_registry/',
-    contactUrl: 'https://github.com/DjSni/kasm_workspaces_registry',
+    listUrl: 'https://registry.kasmweb.com/',
+    contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
   swcMinify: true,
