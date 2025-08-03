01cecd45c6103dda54d83dfe04daa9f5a352220d
DjSni <31524614+DjSni@users.noreply.github.com>
2025-08-03 19:04:48 +0200

    Update Config

--- site/next.config.js
+++ site/next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     contactUrl: 'https://github.com/DjSni/kasm_workspaces_registry',
   },
   reactStrictMode: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/kasm_workspaces_registry/1.1',
   trailingSlash: true,
   images: {
     unoptimized: true,
